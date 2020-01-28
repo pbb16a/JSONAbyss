b265e12495e8bf1fd38c8d385f9de168f11de483
Paula Berggren <pbb16a@acu.edu>
2020-01-28 13:55:16 -0600

    Initial Commit

---
+++ JSON In Class Asmt/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  JSON In Class Asmt
+//
+//  Created by Paula Berggren on 1/28/20.
+//  Copyright © 2020 Paula Berggren. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
